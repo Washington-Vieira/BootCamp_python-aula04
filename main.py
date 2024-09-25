75a1dd59c2798a770d78f31443c43081e3e70992
Washington Vieira <wv817816@gmail.com>
2024-09-25 20:42:13 -0300

    adicionei variaveis, range, lista e for

---
+++ main.py
@@ -0,0 +1,14 @@
+# x = -3
+# if x <= 2:
+#     print("e menor do que zero")
+# elif x <= 00:
+#     print("e menor do que dois")
+# else:
+#     print("More")
+
+# range não gera até o ultimo valor.
+
+# lista = ["fabio", "luciano", "washington"]
+
+# for aluno in lista:
+#     print(aluno)
