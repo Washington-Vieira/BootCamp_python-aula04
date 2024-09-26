29926485b9594fc85f6553ad08dee5e08d8b44f3
Washington Vieira <wv817816@gmail.com>
2024-09-25 22:24:40 -0300

    feat: adicionando o desafio concluido de while e try

--- main.py
+++ main.py
@@ -13,21 +13,23 @@
 # for aluno in lista:
 #     print(aluno)
 
-#estrutura complexa dicionarios
-texto = "Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation."
+# #estrutura complexa dicionarios
+# texto = "Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation."
 
-palavras = texto.split()
-print(palavras)
-contagem_de_palavra = {}
+# palavras = texto.split()
+# print(palavras)
+# contagem_de_palavra = {}
 
-# Eu quero percorrer todas as palavras dentro de palvras e checar se ela ja está no meu conta palavras
+# # Eu quero percorrer todas as palavras dentro de palvras e checar se ela ja está no meu conta palavras
 
-for palavra in palavras:
-    if palavra in contagem_de_palavra:
-        contagem_de_palavra[palavra] = +1
-    else:
-        contagem_de_palavra[palavra] = 1
+# for palavra in palavras:
+#     if palavra in contagem_de_palavra:
+#         contagem_de_palavra[palavra] = +1
+#     else:
+#         contagem_de_palavra[palavra] = 1
 
-hoje = 1
-e = 1
-bootcamp = 2
+# hoje = 1
+# e = 1
+# bootcamp = 2
+
+
