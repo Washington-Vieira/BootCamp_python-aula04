801520943cd57440e31433d08356a8c8c3f641b2
Washington Vieira <wv817816@gmail.com>
2024-09-25 21:08:23 -0300

    Adicionei um dicionario, contador de palavras

--- main.py
+++ main.py
@@ -12,3 +12,22 @@
 
 # for aluno in lista:
 #     print(aluno)
+
+#estrutura complexa dicionarios
+texto = "Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation."
+
+palavras = texto.split()
+print(palavras)
+contagem_de_palavra = {}
+
+# Eu quero percorrer todas as palavras dentro de palvras e checar se ela ja está no meu conta palavras
+
+for palavra in palavras:
+    if palavra in contagem_de_palavra:
+        contagem_de_palavra[palavra] = +1
+    else:
+        contagem_de_palavra[palavra] = 1
+
+hoje = 1
+e = 1
+bootcamp = 2
